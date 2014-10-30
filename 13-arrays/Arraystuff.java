a931f5e43ef974001af926a75da8521d5d963139
tjieluo.capri <tjieluo.capri@gmail.com>
2014-10-29 22:47:27 -0400

    hw

--- 13-arrays/Arraystuff.java
+++ 13-arrays/Arraystuff.java
@@ -23,4 +23,82 @@ public class Arraystuff {
 	}
 	return retd;
     }
+
+    public int sum67(int[] nums) {
+	int retd = 0;
+	for (int i=0; i < nums.length; i++) {
+	    if (nums[i] != 6) {
+		retd = retd + nums[i];
+	    } else {
+		while (nums[i] != 7) {
+		    i++;
+		}
+	    }
+	}
+	return retd;
+    }
+
+    public boolean more14(int[] nums) {
+	int num1 = 0;
+	int num4 = 0;
+	for (int i=0; i < nums.length; i++) {
+	    if (nums[i] == 1) {
+		num1++;
+	    } else if (nums[i] == 4) {
+		num4++;
+	    } else {
+	    }
+	}
+	return (num1 > num4);
+  
+    }
+
+    public int[] tenRun(int[] nums) {
+	int joe = 1;
+	for (int i = 0; i< nums.length; i++) {
+	    if (nums[i] % 10 == 0) {
+		joe = nums[i];
+	    } else if (joe == 1) {
+	    } else {
+		nums[i] = joe;
+	    }
+	}
+	return nums;
+    }
+
+
+    public boolean tripleUp(int[] nums) {
+	for (int i = 0; i < nums.length; i++) {
+	    if (nums.length < 3 || nums.length <= i+2) {
+		return false;
+	    } else if (nums[i] == nums[i+1] - 1 && nums[i] == nums[i+2] - 2) {
+		return true;
+	    } else {
+	    }
+	}
+	return false;
+    }
+
+    public boolean canBalance(int[] nums) {
+	int summa = 0;
+	for (int i = 0; i < nums.length; i++) {
+	    summa = summa + nums[i];
+	}
+	//summa is the sum
+  
+	if (summa % 2 == 0) {
+	    int halb = 0;
+	    for (int i = 0; i < nums.length; i++) {
+		halb = halb + nums[i];
+		if (halb == summa/2) {
+		    return true;
+		}
+	    }
+	} else {
+	    return false;
+	}
+	return false;
+    }
+
+
 }
