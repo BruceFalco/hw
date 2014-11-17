ea2283f7eb6e44ba6faae3aa490f7bc680180426
tjieluo.capri <tjieluo.capri@gmail.com>
2014-11-16 21:32:12 -0500

    hw

--- 17-arraylists/Driver.java
+++ 17-arraylists/Driver.java
@@ -1,8 +1,10 @@
 import java.util.*;
 
+
 public class Driver {
 
     public static void main(String[] args) {
+	//first, for arraylists
 	ArrayList<Integer> al = new ArrayList<Integer> ();
 
 	System.out.println(al);
@@ -22,7 +24,28 @@ public class Driver {
 	}
 
 	System.out.println(ai);
-	     
+	
+	//------
+	//now for arrays
+	int[] rent = new int[10];
+	System.out.println(rent);
+	for (int i = 0; i < rent.length; i++) {
+	    rent[i] = i;
+	}
+	System.out.println(Arrays.toString(rent));
 
+	int[] grease = new int[rent.length];
+	for (int i = 0; i < rent.length; i++) {
+	    int h = 0;
+	    while (grease[h] != 0) {
+		h = r.nextInt(rent.length);
+	    }
+	    grease[h] = rent[i];
+	}
+	rent = grease;
+	
+	System.out.println(Arrays.toString(rent));
+	    
+	
     }
 }
