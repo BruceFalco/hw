1ac46073e473d62c6531022ce223540e97ee4b43
timon.luo <tjieluo.capri@gmail.com>
2014-11-19 09:27:26 -0500

    updating

---
+++ 18-wordsearch/Driver.java
@@ -0,0 +1,8 @@
+public class Driver {
+    public static void main(String[] args) {
+	WordSearch w = new WordSearch();
+	System.out.print(w.toString());
+    }
+}
+
+
---
+++ 18-wordsearch/TwoD.java
@@ -0,0 +1,34 @@
+public class TwoD {
+    public static void main(String[] args) {
+	int[][] a;
+	a = new int [5][3];
+	System.out.println(a.length);
+	//is 5
+	System.out.println(a[0].length);
+	//is 3
+	
+
+	System.out.println();
+
+	a[4][1] = 2;
+	a[1][2] = 9;
+
+	for (int i = 0; i < a.length; i++) {
+	    for (int j = 0; j < a[i].length; j++) {
+		System.out.print(a[i][j]+" ");
+	    }
+	    System.out.println();
+	}
+
+	System.out.println();
+	a[2] = new int[30];
+
+	for (int i = 0; i < a.length; i++) {
+	    for (int j = 0; j < a[i].length; j++) {
+		System.out.print(a[i][j]+" ");
+	    }
+	    System.out.println();
+	}
+
+    }
+}
