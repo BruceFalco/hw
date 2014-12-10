44b8203ffa4825cf92418c44cc95990a14b14900
timon.luo <tjieluo.capri@gmail.com>
2014-12-10 09:12:15 -0500

    classw

--- 21-sortingclasses/Interval.java
+++ 21-sortingclasses/Interval.java
@@ -1,8 +1,41 @@
 import java.util.*;
 
-public class Interval {
+
+    /*
+      Interfaces
+      -> a contract (in java) that specifies that a class will implement
+      certain methods with certain signatures
+     */
+
+/*
+  method signature is: public/private return_type name(params):
+ 
+ for example, the built-in java comparable interface looks something like this:
+ 
+ interface Comparable {
+   public int compareTo(Object other);
+}
+ 
+To agree to implement an interface, use "implements"
+ex:
+
+class myClass implements Comparable {
+ // class definitions here//
+}
+
+A class can only extend one class but it can implement as many interfaces as it wants (separated by ,)
+
+ */
+
+// f(compareTo())
+
+public class Interval{
     private int high,low;
     private static Random r = new Random();
+    // if not static, then will take the same seed in paren and same number
+    //every time
+    // if made static, random gives diff number every time
+
     private static int numIntervals = 0;
 
     private void setup(int l, int h) {
@@ -33,6 +66,7 @@ public class Interval {
 
 
     public int compareTo(Interval other) {
+	
 	if (this.low == other.low) {
 	    return this.high-other.high;
 	} else {
@@ -63,15 +97,24 @@ public class Interval {
 	Interval n1 = new Interval();
 	String nh1 = n1.toString();
 	System.out.println(nh1);
-	
+	Interval n2 = new Interval(10,43);
+	Interval n3 = new Interval(10,60);
+
 	System.out.println(n.compareTo(n1));
+	System.out.println(n3.compareTo(n2));
 
 	printStuff();
 
 	//	Interval[] a = new Interval[10];
 	//	for (int i = 0; i < a.length; i++)
      
-         
+	String[] sa = {"frog","toad","iguana","komodo dragon","bearded lizard"};
+	System.out.println(Arrays.toString(sa));
+	Arrays.sort(sa);
+	System.out.println(Arrays.toString(sa));
 	
     }
+
+
+
 }
