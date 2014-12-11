5b2f60653396e664e6aac3261c123778deb27612
timon.luo <tjieluo.capri@gmail.com>
2014-12-11 09:27:14 -0500

    tb completed hw

--- 22-searching/Searching.java
+++ 22-searching/Searching.java
@@ -3,3 +3,14 @@ import java.util.*;
 public class Searching implements Comparable {
 
     String[] a;
+
+    public Searching() {
+	a = new Searching(20);
+    }
+
+    public Searching(int b) {
+	a = new Searching(b);
+    }
+
+    public void additem(String s) {
+	
